b9bd1ce935e50b272169e3ef21949c8bdba9c4e3
Haykal <ferramentas@haykal.com.br>
2026-06-05 18:05:59 -0300

    Player TakeDamage att

--- Assets/Script/Player/PlayerHealth.cs
+++ Assets/Script/Player/PlayerHealth.cs
@@ -1,23 +1,31 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 3;
+    [SerializeField] private float invincibilityDuration = 2f;
+    [SerializeField] private float blinkInterval = 0.1f;
 
     public event Action<int, int> OnHealthChanged;
     public event Action OnDeath;
     
     private int _currentHealth;
-    private bool _isInvunerable;
+    private bool _isInvulnerable;
+    private MeshRenderer _meshRenderer;
 
-    private void Start() => _currentHealth = maxHealth;
+    private void Start()
+    {
+        _currentHealth = maxHealth;
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
 
-    public void SetInvulnerable(bool value) => _isInvunerable = true;
+    public void SetInvulnerable(bool value) => _isInvulnerable = value;
     
     public void TakeDamage(int damage)
     {
-        if(_isInvunerable) return;
+        if(_isInvulnerable) return;
         
         _currentHealth -= damage;
         OnHealthChanged?.Invoke(_currentHealth, maxHealth);
@@ -26,6 +34,24 @@ public class PlayerHealth : MonoBehaviour
         {
             Die();
         }
+        else
+            StartCoroutine(InvincibilityRoutine());
+    }
+
+    private IEnumerator InvincibilityRoutine()
+    {
+        _isInvulnerable = true;
+
+        float timer = invincibilityDuration;
+        while (timer > 0f)
+        {
+            _meshRenderer.enabled = !_meshRenderer.enabled;
+            yield return new WaitForSeconds(blinkInterval);
+            timer -= blinkInterval;
+        }
+
+        _meshRenderer.enabled = true;
+        _isInvulnerable = false;
     }
 
     private void Die()
